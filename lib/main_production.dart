0f0a6e1ed06f07e2540be54c36f8de211bd38667
zamalkawy1410 <fsafisotricky62@gmail.com>
2024-04-15 21:37:00 +0200

    tgrba

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -4,9 +4,16 @@ import 'package:flutter_complete_project/doc_app.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routing/app_router.dart';
+//hi
+//you
+//are
+//here
+//abdelrahmean
+
 
 void main() async{
   setupGetIt();
+
   // To fix texts being hidden bug in flutter_screenutil in release mode.
   await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
